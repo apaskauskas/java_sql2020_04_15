db7a43c35ee0485fd9acc94c39f2fb6f4824c196
apaskauskas <apaskauskas@hotmail.co.uk>
2020-04-20 12:11:24 +0300

    2020 04 20 pirma diena uzduotis 2, pakoreguotas kodas

--- Example01/src/Example.java
+++ Example01/src/Example.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
 /**
-2020 04 15 Andrius Paskauskas pirma uzduotis
-*/
+ * 2020 04 15 Andrius Paskauskas pirma uzduotis
+ */
 public class Example {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
 
         Scanner sc = new Scanner(System.in);
@@ -14,22 +13,23 @@ public class Example {
             System.out.println("Iveskite zodi, jei ivesite zodi pabaiga programa baigsis: ");
             word = sc.nextLine();
             checkIfWordIsEven(word);
+            System.out.println("A raidziu zodyje yra: " + countLetter(word, 'a'));
 
-        }while(!"pabaiga".equalsIgnoreCase(word));
+        } while (!"pabaiga".equalsIgnoreCase(word));
     }
-    private static void checkIfWordIsEven(String word)
-    {
-       if(word.length() % 2 == 0)
-       {
-           System.out.println(String.format("Ivestas zodis %s yra lyginis", word));
-       } else{
-           System.out.println(String.format("Ivestas zodis %s yra nelyginis", word));
-       }
+
+    private static void checkIfWordIsEven(String word) {
+        if (word.length() % 2 == 0) {
+            System.out.println(String.format("Ivestas zodis %s yra lyginis", word));
+        } else {
+            System.out.println(String.format("Ivestas zodis %s yra nelyginis", word));
+        }
     }
-    private static int countLetter(String word, char letter){
+
+    private static int countLetter(String word, char letter) {
         int count = 0;
-        for(int i = 0; i< word.length(); i++){
-            if(letter == word.charAt(i)){
+        for (int i = 0; i < word.length(); i++) {
+            if (letter == word.charAt(i)) {
                 count++;
             }
         }
